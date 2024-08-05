fa4f35d6423582bbbc37937ea1da6ce34b5f2e36
Joapozzo <pozzojoa@gmail.com>
2024-08-05 18:28:00 -0300

    ultia prueba

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,5 @@
-// export const URL = "https://api-cr.vercel.app"
-export const URL = "http://localhost:3001"
+export const URL = "https://api-coparelampago.vercel.app"
+// export const URL = "http://localhost:3001"
 
 
 
