ccecdf10fb48142618060ce1573cf29fe939b494
perezmarianopm <perezmariano.pm@gmail.com>
2024-08-08 16:28:18 -0300

    cambios de ruta

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,7 @@
 
-// export const URL = "https://api-coparelampago.vercel.app"
+export const URL = "https://api-coparelampago.vercel.app"
 export const URLImages = "https://coparelampago.com"
-export const URL = "http://localhost:3001"
+//export const URL = "http://localhost:3001"
 
 
 
